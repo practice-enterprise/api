19a2f320fd1e0e31696ea974b5a0ad89875f1c53
xunafay <me@xunafay.io>
2020-12-09 14:58:01 +0100

    feat: update and insert guild configs

--- src/controllers/guild.ts
+++ src/controllers/guild.ts
@@ -5,10 +5,21 @@ export class GuildController {
   static router(): Router {
     return Router({ caseSensitive: false })
       .get('/:id', async (req, res, next) => {
-        console.log(req.params.id);
         sofa.db.guilds.get(req.params.id)
           .then((g) => res.send(g))
           .catch(() => res.sendStatus(404))
+          .finally(() => next());
+      })
+      .put('/', (req, res, next) => {
+        sofa.db.guilds.insert(req.body)
+          .then((d) => res.send(d.rev))
+          .catch(() => res.sendStatus(500))
+          .finally(() => next());
+      })
+      .post('/', (req, res, next) => {
+        sofa.db.guilds.insert(req.body)
+          .then((d) => res.send(d.rev))
+          .catch(() => res.sendStatus(500))
           .finally(() => next());
       });
   }
