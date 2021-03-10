8b826a30ad4d99ab37bc7d384ec01f1588427709
DrunkenToast <peter5leconte@gmail.com>
2021-03-10 16:31:41 +0100

    Canvas (Instance) controller

--- src/models/canvas.ts
+++ src/models/canvas.ts
@@ -2,5 +2,5 @@ export interface CanvasInstance {
   _id: string,
   endpoint: string,
   /**CourseID key + last announcement ID*/
-  lastAnnounce: Record<string, string>,
+  lastAnnounce: Record<number, number>,
 }
