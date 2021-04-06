dafe53fb880cd14f5308826d83c28dac23ed3b2c
xunafay <me@xunafay.io>
2021-04-06 13:43:48 +0200

    chore: delete duplicate service

--- src/services/discord-service.ts
+++ src/services/discord-service.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 import { UserGuild } from "../models/discord";
 
-
 export class DiscordService {
-
-  static async getGuilds(token: string) {
-    const guilds = await axios.request<UserGuild[]>({
+  static async getGuilds(token: string): Promise<UserGuild[] | undefined> {
+    return await axios.request<UserGuild[]>({
       headers: {
         Authorization: token
       },
       method: 'GET',
       baseURL: 'https://discord.com/api/v8',
       url: '/users/@me/guilds'
-    }).then((res) => res != null ? res.data : undefined).catch((err) => { console.error(err) });
+    })
+      .then((res) => res != null ? res.data : undefined);
   }
 }
