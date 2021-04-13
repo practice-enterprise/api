e59a7f3c364dab9785d14019ce3f6207ad865934
panda jacobs <jokke.jacobs@gmail.com>
2021-04-13 23:07:57 +0200

    refactor: add return type +throw errors + remove 1liners

--- src/services/channel-creation-service.ts
+++ src/services/channel-creation-service.ts
@@ -5,19 +5,22 @@ import { WebSocket } from "../app";
 
 
 export class ChannelCreationService {
-  static async CreateChannels(discordID: string, guildID: string) {
+  static async CreateChannels(discordID: string, guildID: string): Promise<void> {
     /*find user in db
     get user guild config
     get courses of user from Guild*/
     const config = (await db.collection(Collections.guilds).doc(guildID).get()).data() as Guild | undefined;
-    if( config == undefined){return false; }
+    if( config == undefined){
+      throw new Error('no guild config found');
+    }
 
-    const courses = await CanvasController.getCourses(discordID, config.canvasInstanceID)
-    if(courses == undefined){ return false;}
+    const courses = await CanvasController.getCourses(discordID, config.canvasInstanceID);
+    if(courses == undefined){ 
+      throw new Error('no courses found');
+    }
     let courseIDName: Record<string, string> = {};
     courses.map((course) => courseIDName[course.id] = course.name);
 
     WebSocket?.sendForGuild(guildID, 'createChannels', {'guildID':guildID, 'courseChannels': config.courseChannels, 'courses': courseIDName});
-    return true;
   }
 }
