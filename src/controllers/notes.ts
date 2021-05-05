3cdeea0e8726112094180f06b05bca6b578bc7e1
DrunkenToast <peter5leconte@gmail.com>
2021-05-05 23:27:23 +0200

    feat/refactor: DM working, DB changes

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -4,14 +4,20 @@ import { Collections, db } from '../services/database';
 export class NotesController {
   static router(): Router {
     return Router({ caseSensitive: false })
-      .get('/', (req, res, next) => {
-        db.collection(Collections.notes).get()
-          .then((snapshot) => res.send(snapshot.docs.map((d) => d.data())))
-          .finally(() => next());
+      .get('/:id', async (req, res, next) => {
+        const noteSnapshot = await db.collection(Collections.notes)
+          .where('id', '==', req.params.id).get()
+        if (noteSnapshot.empty) {
+          res.sendStatus(404);
+          next();
+        }
+        res.send(noteSnapshot.docs[0].data());
+        next();
       })
+
       .put('/', (req, res, next) => {
         db.collection(Collections.notes)
-          .doc(req.body.channel)
+          .doc(req.body.id)
           .set(req.body)
           .then(() => res.sendStatus(204))
           .finally(() => next());
